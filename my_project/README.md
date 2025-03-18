7ef482e523d690af3899d85bb6d63f8197b20851
IanKim <ihwan1236@gmail.com>
2025-03-18 16:57:58 -0700

    HTTP, GET Mothod: to fetch data from  mock server

--- my_project/README.md
+++ my_project/README.md
@@ -2,7 +2,7 @@
 
 ## Command List
 
-~~~
+~~~shell
 npm init
 npm install serve
 
@@ -21,8 +21,28 @@ vi package.json ( add "static" under the "scripts" )
 npm run static
 
 // Fetch data from another server not mockData
-> edit app.js
+>> edit app.js
 touch server.mjs
 node server.mjs // Run data server
 npm run static // Run main server
+~~~
+
+~~~shell
+// Create the mock service using the Fastify framework
+// Node.js web framework that is built for rapid implementation and high performance
+mkdir mock-srv
+sd mock-srv
+npm add fastify fastify-cli
+npx fastify generate . --esm
+npm install
+npm install @fastify/cors
+>> edit app.js in mock-srv
+>> * fastify.register(cors)
+
+>> create index.js in mock-srv/routes/[item]
+
+// Run web server in 5050 port
+cd .. && npm run static
+// Run data server in 3000 port
+npm run dev
 ~~~
--- my_project/static/app.js
+++ my_project/static/app.js
@@ -1,15 +1,18 @@
 const API = "http://localhost:3000"
 
-const data = [
-    {id: "A1", name: "Vacum Cleaner", rrp: "99.99", info: "The most powerful vacuum in ther world."},
-    {id: "A2", name: "Leaf Blower", rrp: "303.33", info: "This product will blow your socks off."},
-    {id: "B1", name: "Chocolate Bar", rrp: "22.40", info: "Deliciously overpriced chocolate."},
-]
-const populateProducts = async () => {
+// const data = [
+//     {id: "A1", name: "Vacum Cleaner", rrp: "99.99", info: "The most powerful vacuum in ther world."},
+//     {id: "A2", name: "Leaf Blower", rrp: "303.33", info: "This product will blow your socks off."},
+//     {id: "B1", name: "Chocolate Bar", rrp: "22.40", info: "Deliciously overpriced chocolate."},
+// ]
+const populateProducts = async (category) => {
     const products = document.querySelector("#products")
     products.innerHTML = ""
-    const res = await fetch(API)
+
+    const res = await fetch(`${API}/${category}`)
+    console.log(res);
     const data = await res.json()
+
     for (const product of data) {
         const item = document.createElement("product-item")
         for (const key of ["name", "rrp", "info"]) {
@@ -22,9 +25,15 @@ const populateProducts = async () => {
     }
 }
 
-document.querySelector("#fetch").addEventListener("click", async () => {
-    await populateProducts()
+const category = document.querySelector("#category")
+
+category.addEventListener('input', async ({ target }) => {
+    await populateProducts(target.value)
 })
+
+// document.querySelector("#fetch").addEventListener("click", async () => {
+//     await populateProducts()
+// })
 
 customElements.define("product-item", class Item extends HTMLElement {
     constructor() {
